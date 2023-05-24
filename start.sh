979f4dfd25e0d7aae7a57a302bb71bb72c9b0886
Aaron Bieber <aaron@aaronbieber.com>
2023-05-24 06:43:11 -0400

    Use absolute path for Docker volumes

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run -v ./request_cache:/src/request_cache \
-           -v ./tokens:/src/tokens \
+docker run -v ${PWD}/config.py:/src/config.py \
+           -v ${PWD}/request_cache:/src/request_cache \
+           -v ${PWD}/tokens:/src/tokens \
            -w /src \
            enlighten python monitor.py
